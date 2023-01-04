0d7bb0a39a1323f3c6f30b6e3a8961f3f332b20a
micgreene <mgreene11111@yahoo.com>
2023-01-04 21:29:29 +0000

    join and split

---
+++ ChallengeLabs/challenge18.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    username = input("What's your name? : ")
+    dayofweek = input("What is today? : ")
+    print("Hello, ",username,"! Happy ",dayofweek,"!", sep="")
+
+if __name__ == "__main__":
+    main()
