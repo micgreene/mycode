13d7c731fd97beecc2e70b144440c922ba23eead
micgreene <mgreene11111@yahoo.com>
2023-01-05 16:48:00 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# list of animals
+barnyard = ['dog', 'bat', 'cat', 'cow', 'pig']
+
+print(' '.join(map(str, barnyard))) # prints a copy of the list as strings with no commas by using map to join all list itmes into one string
+# 
